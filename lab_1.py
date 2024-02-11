0bed13276a67ed14a204f9d80bd6d9029f7459c6
KopanetskayaAnastasia <kopanetskaya.anastasia@yandex.ru>
2024-02-11 16:12:32 +0300

    Second commit

--- lab_1.py
+++ lab_1.py
@@ -1,4 +1,5 @@
 import random
+import re
 from array import *
 
 
@@ -44,7 +45,7 @@ def func_3(n):
         comp_number *= n1 % 10
         n1 //= 10
     del_n = array('i', [])
-    for i in range(1, n):
+    for i in range(1, n + 1):
         if n % i == 0:
             del_n.append(i)
     summ = 0
@@ -64,4 +65,52 @@ def func_3(n):
 num = int(input('Enter number: '))
 print(func_3(num))
 
+
+#Task2-4.v5.5: дана строка, перемешать все символы в случайном порядке
+def func_5(n):
+    a = []
+    for i in n:
+        a.append(i)
+    random.shuffle(a)
+    s = ''
+    for i in a:
+        s += i
+    return s
+
+
+num = str(input('Enter string: '))
+print(func_5(num))
+
+
+#Task2-4.v5.7: дана строка, состоящая из символов латиницы. необходимо проверить, образуют ли прописные символы этой строки палиндром
+def func_7(n):
+    k = True
+    for i in n:
+        if not re.search(r'[A-Za-z\s]', i):
+            k = False
+    s = ''
+    if k is True:
+        for i in n:
+            if i.isupper():
+                s += i
+    else:
+        return "Error, use only Latin letters"
+    return s == s[::-1]
+
+
+num = str(input('Is it palindrome: '))
+print(func_7(num))
+
+
+#Task2-3.v5.14: дана строка, в которой записаны слова через пробел. необх упоряд слова по кол-ву букв в каждом слове
+def func_14(n):
+    s = n.split()
+    k = ''
+    for i in sorted(s, key=len):
+        k += i
+        k += ' '
+    return k
+
+num = str(input('Enter string: '))
+print(func_14(num))
 
